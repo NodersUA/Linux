89dfb39b7d1a779496c210f60d01f98d7863ea9a
MYKHAILO SEMENIUK <79005788+muhaylosemenyuk@users.noreply.github.com>
2023-04-01 15:19:07 +0300

    Update firewall.sh

--- firewall/firewall.sh
+++ firewall/firewall.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-apt-get install -y ufw
+apt-get install -y ufw && apt-get install -y fail2ban
 
 ufw allow 22
 ufw allow 80
 ufw allow 443
-
-ufw enable
 
 ufw deny out from any to 10.0.0.0/8
 ufw deny out from any to 172.16.0.0/12
@@ -15,5 +13,4 @@ ufw deny out from any to 100.64.0.0/10
 ufw deny out from any to 198.18.0.0/15
 ufw deny out from any to 169.254.0.0/16
 
-apt-get install -y fail2ban
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
