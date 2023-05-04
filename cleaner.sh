c548fff3bf902d9f43a673fcc3d420de7c894feb
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-04 15:51:04 +0300

    Create cleaner.sh

---
+++ cleaner.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Add cleaner.sh to scripts folder
+tee $HOME/scripts/cleaner.sh > /dev/null <<EOF
+#!/bin/bash
+while true
+do
+    date
+    # remove syslog
+    rm /var/log/syslog*
+    # remove subspace log files
+    rm $HOME/.local/share/subspace-cli/logs/*
+    sleep 600
+    echo "===================================="
+done
+EOF
+
+chmod +x $HOME/scripts/cleaner.sh
+
+# Create logs cleaner service file
+sudo tee /etc/systemd/system/cleaner.service > /dev/null <<EOF
+[Unit]
+Description=Logs Cleaner Service
+After=network.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/scripts/cleaner.sh
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start cleaner service file
+systemctl daemon-reload
+systemctl enable cleaner
+systemctl restart cleaner
